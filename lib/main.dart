0ff4d92c2c76fad6c41a7dba50070eb5de3b7e54
seonhannn <seon.hannn@gmail.com>
2022-12-17 19:37:33 +0900

    ThemeData 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'style.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      theme: theme,
       home: MyApp()
     )
   );
@@ -14,7 +16,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text("Instagram"),
+        actions: [
+          Icon(Icons.add),
+          SizedBox(width: 20),
+          Icon(Icons.favorite_border),
+          SizedBox(width: 20),
+          Icon(Icons.send)
+        ],
+      ),
+      body: Container(
+        
+      )
     );
   }
 }
---
+++ lib/style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+var theme = ThemeData(
+    appBarTheme: AppBarTheme(
+      color: Colors.white,
+      elevation: 1,
+      titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+      actionsIconTheme: IconThemeData(color: Colors.black),
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        backgroundColor: Colors.grey
+      )
+    )
+);
